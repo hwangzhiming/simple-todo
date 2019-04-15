7f09780020300ac481c831ee7ea2657470b6df70
Alex Huang <hwangzhiming@gmail.com>
2019-04-16 00:46:12 +0800

    update

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -22,7 +22,7 @@ export class TodoListComponent implements OnInit {
   ngOnInit() {
     this.storeService.todo$.subscribe((data: Todo[]) => {
       this.allTodos = data;
-      this.title.setTitle(`TODO (${this.remainCount}/${this.allTodos.length})`);
+      this.title.setTitle(`Simple Todo (${this.remainCount}/${this.allTodos.length})`);
     });
 
     this.storeService.restore().subscribe(() => {
